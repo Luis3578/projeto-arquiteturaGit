42effa1807b3758d5ebf46487606e27d7f98bef6
Luis3578 <luispess75@gmail.com>
2026-05-16 04:38:35 -0300

    Projeto concluido!

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
